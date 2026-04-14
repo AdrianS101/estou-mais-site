81c74b663405b3162c8ce3f2baac4a5e434febb5
AdrianS101 <adriansouzasilva5894@gmail.com>
2026-04-14 15:27:08 -0300

    atualiza imagens

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Clientes } from '../../components/clientes/clientes';
+import { Component } from '@angular/core';
+import { AgentTeams } from '../../components/agent-teams/agent-teams';
+import { Campaigns } from '../../components/campaigns/campaigns';
+import { ChatDemo } from '../../components/chat-demo/chat-demo';
+import { ChatWidget } from '../../components/chat-widget/chat-widget';
+import { CustomizeSolution } from '../../components/customize-solution/customize-solution';
+import { FollowUp } from '../../components/follow-up/follow-up';
+import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
+import { Hero } from '../../components/hero/hero';
+import { Pricing } from '../../components/pricing/pricing';
+import { SentimentAnalysis } from '../../components/sentiment-analysis/sentiment-analysis';
+import { Workflow } from '../../components/workflow/workflow';
+
+
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    Header,
+    Hero,
+    Clientes,
+    ChatDemo,
+    Workflow,
+    FollowUp,
+    SentimentAnalysis,
+    AgentTeams,
+    Campaigns,
+    Pricing,
+    CustomizeSolution,
+    Footer,
+    ChatWidget,
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class HomePage {
+  title = 'ESTOU+';
+}
